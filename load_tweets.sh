168e6a235c1739ea1c77cd4d8f6106e0124cfd0c
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-04-07 21:13:07 -0700

    Updated load_tweets.sh

--- load_tweets.sh
+++ load_tweets.sh
@@ -3,18 +3,18 @@
 # list all of the files that will be loaded into the database
 # for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
 # but we will write are code so that we can easily load an arbitrary number of files
-files='
+files="
 test-data.zip
-'
+"
 
-echo 'load normalized'
+echo "load normalized"
 for file in $files; do
-    python3 load_tweets.py --db postgresql://postgres:pass@localhost:6556/postgres --inputs "$file"
+    echo "Processing normalized file: $file"
+    python3 load_tweets.py --db "postgresql://postgres:pass@localhost:6556/postgres" --inputs "$file"
 done
 
-echo 'load denormalized'
+echo "load denormalized"
 for file in $files; do
-    # use SQL's COPY command to load data into pg_denormalized
-    # cat <JSONFILE> | sed 's/\\u0000//g' | psql <POSTGRES_DB_URL> -c "COPY <TABLENAME> (<COLUMNNAME>) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
-    #cat "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:6555 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    echo "Processing denormalized file: $file"
+    cat "$file" | sed 's/\\u0000//g' | psql "postgresql://postgres:pass@localhost:6555" -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
